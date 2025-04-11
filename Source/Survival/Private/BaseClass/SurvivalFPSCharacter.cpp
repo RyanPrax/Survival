286c2dbb0f072a1bbcb196ce99f1541619b67bce
RyanPrax <140210909+RyanPrax@users.noreply.github.com>
2025-04-10 20:15:44 -0500

    initial commit

---
+++ Source/Survival/Private/BaseClass/SurvivalFPSCharacter.cpp
@@ -0,0 +1,61 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BaseClass/SurvivalFPSCharacter.h"
+#include "Components/StatlineComponent.h"
+
+// Sets default values
+ASurvivalFPSCharacter::ASurvivalFPSCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Statline = CreateDefaultSubobject<UStatlineComponent>(TEXT("Statline"));
+	Statline->SetMovementCompReference(GetCharacterMovement());
+}
+
+
+// Called when the game starts or when spawned
+void ASurvivalFPSCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+bool ASurvivalFPSCharacter::CanJump() const
+{
+	return Statline->CanJump();
+}
+
+
+void ASurvivalFPSCharacter::HasJumped()
+{
+	Statline->HasJumped();
+	ACharacter::Jump();
+}
+
+bool ASurvivalFPSCharacter::CanSprint() const
+{
+	return Statline->CanSprint();
+}
+
+void ASurvivalFPSCharacter::SetSprinting(const bool& IsSprinting)
+{
+	Statline->SetSprinting(IsSprinting);
+}
+
+
+// Called every frame
+void ASurvivalFPSCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ASurvivalFPSCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
---
+++ Source/Survival/Public/Components/StatlineComponent.h
@@ -0,0 +1,148 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "StatlineComponent.generated.h"
+
+
+UENUM(BlueprintType)
+enum class ECoreStat : uint8 //Unreal requires ECoreStat to derive from uint8
+{
+	// CS for CoreStat
+	// UMETA changes varibale display name
+	CS_HEALTH UMETA(DisplayName="Health"),
+	CS_STAMINA UMETA(DisplayName = "Stamina"),
+	CS_HUNGER UMETA(DisplayName = "Hunger"),
+	CS_THIRST UMETA(DisplayName = "Thirst"),
+	CS_TEMPERATURE UMETA(DisplayName = "Temperature"),
+};
+
+
+USTRUCT(BlueprintType)
+struct FCoreStat
+{
+	GENERATED_USTRUCT_BODY()
+
+private:
+	// meta = AllowPrivateAccess allows you to see the private variable
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	float Current = 100;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	float Max = 100;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	float PerSecondTick = 1;
+
+public:
+
+	FCoreStat()
+	{
+	};
+	FCoreStat(const float& current, const float& max, const float& tick)
+	{
+		Current = current;
+		Max = max;
+		PerSecondTick = tick;
+	}
+	
+	// & is reference, so that we dont have hundreds of copies of the value
+	// We arent modifying anyways
+	void TickStat(const float& DeltaTime)
+	{
+		Current = FMath::Clamp(Current + (PerSecondTick * DeltaTime), 0, Max);
+	}
+
+	// Works for both adding and subtracting
+	void Adjust(const float& Amount)
+	{
+		Current = FMath::Clamp(Current + Amount, 0, Max);
+	}
+
+	// Returns the percentile of the stat for displaying as a progress bar UI Element
+	float Percentile() const
+	{
+		return Current / Max;
+	}
+
+	// Used for Hunger/Thirst because these decay over time
+	void AdjustTick(const float& NewTick) 
+	{
+		PerSecondTick = NewTick;
+	}
+
+	float GetCurrent() const
+	{
+		return Current;
+	}
+
+
+};
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class SURVIVAL_API UStatlineComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+private:
+
+	class UCharacterMovementComponent* OwningCharacterMovementComp;
+
+	// Energy
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	FCoreStat Health;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	FCoreStat Stamina;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	FCoreStat Hunger = FCoreStat(100, 100, -0.125);
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	FCoreStat Thirst = FCoreStat(100, 100, -0.25);
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	bool bIsSprinting = false;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	float SprintCostMultiplier = 2;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	float WalkSpeed = 125;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	float SprintSpeed = 450;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	float JumpCost = 10;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	float SecondsForStaminaExhaustion = 3;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	float CurrentStaminaExhaustion = 0;
+
+	void TickStats(const float& DeltaTime);
+	void TickStamina(const float& DeltaTime);
+	bool IsValidSprinting();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Sets default values for this component's properties
+	UStatlineComponent();
+
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	UFUNCTION(BlueprintCallable)
+	void SetMovementCompReference(UCharacterMovementComponent* Comp);
+
+	UFUNCTION(BlueprintCallable)
+	float GetStatPercentile(const ECoreStat Stat) const;
+
+    UFUNCTION(BlueprintCallable)
+    bool CanSprint() const;
+	UFUNCTION(BlueprintCallable)
+	void SetSprinting(const bool& isSprinting);
+	UFUNCTION(BlueprintCallable)
+	bool CanJump();
+	UFUNCTION(BlueprintCallable)
+	void HasJumped();
+};
